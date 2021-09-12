aa24d35cabf703b0ecdba5793e4f5a1db74eccae
thisistaimur <59065240+thisistaimur@users.noreply.github.com>
2021-09-12 23:21:34 +0200

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require('express');
 const bodyparser = require('body-parser');
 
 const app = express();
-const port = process.env.$PORT;
+const port = process.env.PORT;
 
 // serve files from the public directory
 app.use(express.static('public'));
